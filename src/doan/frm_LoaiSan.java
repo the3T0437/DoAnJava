c23e9c828a22c5a8a03edab116bdb15fb00805af
The3T <thanhthuong12032005@gmail.com>
2024-05-17 08:31:05 +0700

    them frm_LoaiSan

--- src/doan/frm_LoaiSan.java
+++ src/doan/frm_LoaiSan.java
@@ -4,6 +4,11 @@
  */
 package doan;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
 /**
  *
  * @author VINH
@@ -26,17 +31,140 @@ public class frm_LoaiSan extends javax.swing.JFrame {
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
+                jLabel1 = new javax.swing.JLabel();
+                jLabel2 = new javax.swing.JLabel();
+                jLabel3 = new javax.swing.JLabel();
+                tf_DonGia = new javax.swing.JTextField();
+                tf_MaLoaiSan = new javax.swing.JTextField();
+                btn_luu = new javax.swing.JButton();
+                btn_doc = new javax.swing.JButton();
+                btn_them = new javax.swing.JButton();
+                btn_xoa = new javax.swing.JButton();
+                btn_sua = new javax.swing.JButton();
+                lbl_thongBao = new javax.swing.JLabel();
+                jScrollPane2 = new javax.swing.JScrollPane();
+                table_output = new javax.swing.JTable();
+
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+                jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 24)); // NOI18N
+                jLabel1.setForeground(new java.awt.Color(153, 255, 255));
+                jLabel1.setText("Quản Lý Loại Sân Banh");
+
+                jLabel2.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                jLabel2.setText("Mã Loại Sân:");
+
+                jLabel3.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                jLabel3.setText("Đơn Giá:");
+
+                tf_DonGia.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+
+                tf_MaLoaiSan.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+
+                btn_luu.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                btn_luu.setText("Lưu");
+
+                btn_doc.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                btn_doc.setText("Đọc");
+
+                btn_them.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                btn_them.setText("Thêm");
+
+                btn_xoa.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                btn_xoa.setText("Xoá");
+
+                btn_sua.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                btn_sua.setText("Sửa");
+
+                lbl_thongBao.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+                lbl_thongBao.setForeground(new java.awt.Color(204, 0, 51));
+                lbl_thongBao.setText("Thông báo");
+
+                table_output.setModel(new javax.swing.table.DefaultTableModel(
+                        new Object [][] {
+
+                        },
+                        new String [] {
+                                "Mã Loại Sân", "Đơn Giá"
+                        }
+                ) {
+                        Class[] types = new Class [] {
+                                java.lang.String.class, java.lang.String.class
+                        };
+                        boolean[] canEdit = new boolean [] {
+                                false, false
+                        };
+
+                        public Class getColumnClass(int columnIndex) {
+                                return types [columnIndex];
+                        }
+
+                        public boolean isCellEditable(int rowIndex, int columnIndex) {
+                                return canEdit [columnIndex];
+                        }
+                });
+                jScrollPane2.setViewportView(table_output);
 
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
                 getContentPane().setLayout(layout);
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 889, Short.MAX_VALUE)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(27, 27, 27)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addComponent(jLabel2)
+                                                        .addComponent(jLabel3))
+                                                .addGap(33, 33, 33)
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                                        .addComponent(tf_DonGia, javax.swing.GroupLayout.PREFERRED_SIZE, 403, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(tf_MaLoaiSan, javax.swing.GroupLayout.PREFERRED_SIZE, 403, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(btn_them)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btn_xoa)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btn_sua)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btn_luu)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btn_doc))
+                                        .addComponent(lbl_thongBao))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 469, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(25, 25, 25))
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(452, 452, 452)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 273, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 543, Short.MAX_VALUE)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel1)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                        .addComponent(jLabel2)
+                                                        .addComponent(tf_MaLoaiSan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                .addGap(18, 18, 18)
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                        .addComponent(jLabel3)
+                                                        .addComponent(tf_DonGia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                .addGap(18, 18, 18)
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                        .addComponent(btn_them)
+                                                        .addComponent(btn_xoa)
+                                                        .addComponent(btn_sua)
+                                                        .addComponent(btn_luu)
+                                                        .addComponent(btn_doc))
+                                                .addGap(26, 26, 26)
+                                                .addComponent(lbl_thongBao))
+                                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 265, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(39, Short.MAX_VALUE))
                 );
 
                 pack();
@@ -78,5 +206,54 @@ public class frm_LoaiSan extends javax.swing.JFrame {
     }
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JButton btn_doc;
+        private javax.swing.JButton btn_luu;
+        private javax.swing.JButton btn_sua;
+        private javax.swing.JButton btn_them;
+        private javax.swing.JButton btn_xoa;
+        private javax.swing.JLabel jLabel1;
+        private javax.swing.JLabel jLabel2;
+        private javax.swing.JLabel jLabel3;
+        private javax.swing.JScrollPane jScrollPane2;
+        private javax.swing.JLabel lbl_thongBao;
+        private javax.swing.JTable table_output;
+        private javax.swing.JTextField tf_DonGia;
+        private javax.swing.JTextField tf_MaLoaiSan;
         // End of variables declaration//GEN-END:variables
+
+	public JButton getBtn_doc() {
+		return btn_doc;
+	}
+
+	public JButton getBtn_luu() {
+		return btn_luu;
+	}
+
+	public JButton getBtn_sua() {
+		return btn_sua;
+	}
+
+	public JButton getBtn_them() {
+		return btn_them;
+	}
+
+	public JButton getBtn_xoa() {
+		return btn_xoa;
+	}
+
+	public JLabel getLbl_thongBao() {
+		return lbl_thongBao;
+	}
+
+	public JTable getTable_output() {
+		return table_output;
+	}
+
+	public JTextField getTf_DonGia() {
+		return tf_DonGia;
+	}
+
+	public JTextField getTf_MaLoaiSan() {
+		return tf_MaLoaiSan;
+	}
 }
